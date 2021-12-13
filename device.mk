f10e3a86184f94efc50da702132df42eed84452f
Pranav Vashi <neobuddy89@gmail.com>
2021-12-14 01:09:04 +0530

    vince: camera: build 32-bit android.frameworks.displayservice@1.0
    Signed-off-by: adnan-44 <mohd.adnan0405@gmail.com>
    Signed-off-by: rk134 <rahul-k@bigdi.cc>

--- device.mk
+++ device.mk
@@ -113,6 +113,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.frameworks.displayservice@1.0_32 \
     android.hardware.camera.common@1.0 \
     android.hardware.camera.provider@2.4 \
     android.hardware.camera.provider@2.4-impl \
